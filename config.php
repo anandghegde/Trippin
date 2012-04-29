90ab5fe55486ccb976906ae957fe0fdb4534cbe8
Anand Hegde <anandghegde@gmail.com>
2012-04-29 12:55:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
 
-$db_host = "mysql1.alwaysdata.com";
+$db_host = "";
 // Place the username for the MySQL database here
-$db_username = "anandghegde"; 
+$db_username = ""; 
 // Place the password for the MySQL database here
-$db_pass = "se123";
+$db_pass = "";
 // Place the name for the MySQL database here
-$db_name = "anandghegde_se";
+$db_name = "";
 
 mysql_connect("$db_host","$db_username","$db_pass") or die(mysql_error());
 mysql_select_db("$db_name") or die("no database by that name");
