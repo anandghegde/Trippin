1f61e2092aadb866f270264fc72abb09582ba775
Anand Hegde <anandghegde@gmail.com>
2012-04-29 12:57:24 +0530

    Update rate.php

--- rate.php
+++ rate.php
@@ -2,13 +2,13 @@
 session_start(); 
 
 
-$db_host = "mysql1.alwaysdata.com";
+$db_host = "";
 // Place the username for the MySQL database here
-$db_username = "anandghegde"; 
+$db_username = ""; 
 // Place the password for the MySQL database here
-$db_pass = "se123";
+$db_pass = "";
 // Place the name for the MySQL database here
-$db_name = "anandghegde_se";
+$db_name = "";
 
 $connection = mysql_connect($db_host,$db_username,$db_pass) or die(mysql_error());
 mysql_select_db($db_name,$connection) or die("no database by that name");
